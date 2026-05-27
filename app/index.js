5e745361f5ac611e2044c2ba34ab7c3d5a755097
Otavio-MBarra <otaviombarra@gmail.com>
2026-05-27 12:34:51 -0300

    feat: render transaction list dynamically from array

--- app/index.js
+++ app/index.js
@@ -3,10 +3,21 @@ import Transaction from "./Transaction.js";
 const form = document.getElementById("transaction-form");
 let getTransationsJSON = localStorage.getItem("transactions");
 let transactions = JSON.parse(getTransationsJSON) || [];
+const transactionsList = document.querySelector(".transactionsList");
 const cleanBtn = document.getElementById("clean-local");
 cleanBtn.addEventListener("click", () => {
   localStorage.clear();
 });
+
+// transactionsList.innerHTML = `<div class="recordedTransaction">
+//         <img src="https://picsum.photos" alt="" />
+//         <div class="descriptionTransaction">
+//           <p class="descriptionTransactionText">salario</p>
+//           <p class="transactionDate">25 jun</p>
+//           <p class="transactionCategory">lazer</p>
+//         </div>
+//         <p class="transactionValueText">+R$ 1000,00</p>
+//       </div>`;
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -26,4 +37,17 @@ form.addEventListener("submit", (event) => {
   localStorage.setItem("transactions", JSON.stringify(transactions));
   console.log(transactions);
 });
-console.log(transactions);
+
+transactions.forEach((transaction) => {
+  transactionsList.innerHTML = `<div class="recordedTransaction">
+        <img src="https://picsum.photos" alt="" />
+        <div class="descriptionTransaction">
+          <p class="descriptionTransactionText">${transaction.description}</p>
+          <p class="transactionDate">${transaction.date}</p>
+          <p class="transactionCategory">${transaction.category}</p>
+        </div>
+        <p class="transactionValueText">${transaction.value}</p>
+      </div>`;
+});
+
+transactions.reduce((acc, curent) => {});
