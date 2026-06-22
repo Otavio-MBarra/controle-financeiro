ada74dee5f6e6a22c9f6b77d9ff81ae7844d38d6
Otavio-MBarra <otaviombarra@gmail.com>
2026-06-22 14:21:19 -0300

    feat: add financial summary metrics

--- app/BalanceRenderer.js
+++ app/BalanceRenderer.js
@@ -1,8 +1,21 @@
-export default function balanceRenderer(transactions, h2ShowBalance) {
+export default function balanceRenderer(
+  transactions,
+  h2Total,
+  pExpense,
+  pRecipe,
+) {
   let balance = transactions.reduce((acc, current) => {
-    return current.incomeexpense === "income"
-      ? (acc += parseFloat(current.value))
-      : (acc -= parseFloat(current.value));
-  }, 0);
-  h2ShowBalance.textContent = `R$ ${balance}`;
+    if (acc[current.incomeexpense]) {
+      acc[current.incomeexpense] =
+        parseFloat(current.value) + parseFloat(acc[current.incomeexpense]);
+    } else {
+      acc[current.incomeexpense] = parseFloat(current.value);
+    }
+
+    return acc;
+  }, {});
+  console.log(balance);
+  h2Total.textContent = `R$ ${(balance.income || 0) - (balance.expense || 0)}`;
+  pExpense.textContent = `R$ ${balance.expense || 0}`;
+  pRecipe.textContent = `R$ ${balance.income || 0}`;
 }
--- app/index.js
+++ app/index.js
@@ -7,12 +7,16 @@ let getTransationsJSON = localStorage.getItem("transactions");
 let transactions = JSON.parse(getTransationsJSON) || [];
 const transactionsList = document.querySelector(".transactionsList");
 const h2ShowBalance = document.querySelector(".showBalance");
+const pMonthlyExpenseValue = document.querySelector(".monthly-expenses-value");
+const pRecipeMonthValue = document.querySelector(".recipe-month-value");
 
 const cleanBtn = document.getElementById("clean-local");
 cleanBtn.addEventListener("click", () => {
   localStorage.clear();
   transactionRenderer();
 });
+
+// let transactionValues = balanceRenderer(transactions);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -31,8 +35,18 @@ form.addEventListener("submit", (event) => {
   transactions.unshift(transaction);
   localStorage.setItem("transactions", JSON.stringify(transactions));
   transactionRenderer(transactions, transactionsList);
-  balanceRenderer(transactions, h2ShowBalance);
+  balanceRenderer(
+    transactions,
+    h2ShowBalance,
+    pMonthlyExpenseValue,
+    pRecipeMonthValue,
+  );
 });
 
 transactionRenderer(transactions, transactionsList);
-balanceRenderer(transactions, h2ShowBalance);
+balanceRenderer(
+  transactions,
+  h2ShowBalance,
+  pMonthlyExpenseValue,
+  pRecipeMonthValue,
+);
