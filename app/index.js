1c9231082b85e81631d08ce4a91bee740658b6ed
Otavio-MBarra <otaviombarra@gmail.com>
2026-06-30 12:25:39 -0300

    feat: add installment fields to transaction form

--- app/index.js
+++ app/index.js
@@ -10,16 +10,45 @@ const h2ShowBalance = document.querySelector(".showBalance");
 const pMonthlyExpenseValue = document.querySelector(".monthly-expenses-value");
 const pRecipeMonthValue = document.querySelector(".recipe-month-value");
 
+const radioOptions = document.querySelectorAll(
+  "input[name='transactionInstallment']",
+);
+const credCardOption = document.querySelector(
+  "select[name='transactionPayment']",
+);
+const inputInstalment = document.getElementById("installment-count-wrapper");
+const showInstallmentModal = document.querySelector(".modal-installment");
+
 const cleanBtn = document.getElementById("clean-local");
 cleanBtn.addEventListener("click", () => {
   localStorage.clear();
   transactionRenderer();
 });
 
+credCardOption.addEventListener("change", (element) => {
+  toggleHidden(element, "Cartão de Crédito", showInstallmentModal);
+});
+
+radioOptions.forEach((option) => {
+  option.addEventListener("change", (element) => {
+    toggleHidden(element, "yes", inputInstalment);
+  });
+});
+
+function toggleHidden(element, criteri, toggleElement) {
+  if (element.target.value === criteri) {
+    toggleElement.hidden = false;
+  } else {
+    toggleElement.hidden = true;
+  }
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const data = new FormData(event.target);
   const values = Object.fromEntries(data.entries());
+  console.log(data);
+  console.log(values);
 
   const transaction = new Transaction(
     values.transactionValue,
